fda2cd950ea90ba9f9aa16a817ad4e5af7fce9d2
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-14 19:43:02 -0400

    Update EventoRepository.java

--- api-igreja/src/main/java/com/igreja/api/repository/EventoRepository.java
+++ api-igreja/src/main/java/com/igreja/api/repository/EventoRepository.java
@@ -1,6 +1,7 @@
 package com.igreja.api.repository;
 
 import java.util.List;
+import java.time.LocalDate;
 import com.igreja.api.model.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
