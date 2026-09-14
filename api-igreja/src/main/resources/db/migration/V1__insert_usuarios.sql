7ba8871bea2ca90c95c5dfd70dd849b5f9947b59
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-14 11:41:45 -0400

    Add files via upload

---
+++ api-igreja/src/main/resources/db/migration/V1__insert_usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (nome, email, senha_hash, cargo)
+VALUES ('Pr. Natan Leite', 'natanleitte@gmail.com', '123456', 'Pastor Presidente');
+INSERT INTO usuarios (nome, email, senha_hash, cargo)
+VALUES ('Pr. Jorge Silva', 'jorge.silva.msbr@gmail.com', '123456', 'Pastor Auxiliar');
