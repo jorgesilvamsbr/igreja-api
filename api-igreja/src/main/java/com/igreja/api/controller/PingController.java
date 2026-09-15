77d52c8a2763f5560fce5477349ff97ad9805a12
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-15 18:32:14 -0400

    Create PingController.java

---
+++ api-igreja/src/main/java/com/igreja/api/controller/PingController.java
@@ -0,0 +1,17 @@
+package com.igreja.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+  @GetMapping("/ping")
+  @ResponseStatus(HttpStatus.OK)
+  public String ping() {
+    return "pong";
+  }
+}
