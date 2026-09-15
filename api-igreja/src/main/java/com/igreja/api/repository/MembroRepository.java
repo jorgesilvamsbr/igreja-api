caaacbb1c724901d3349b6da239624535221d9cc
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-15 18:50:24 -0400

    Update MembroRepository.java

--- api-igreja/src/main/java/com/igreja/api/repository/MembroRepository.java
+++ api-igreja/src/main/java/com/igreja/api/repository/MembroRepository.java
@@ -3,7 +3,9 @@ package com.igreja.api.repository;
 import com.igreja.api.model.Membro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.time.LocalDate;
 
 public interface MembroRepository extends JpaRepository<Membro, Long> {
     List<Membro> findByNomeContainingIgnoreCase(String nome);
+    List<Membro> findByDataNascimento(LocalDate dataEvento);
 }
