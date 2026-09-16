ed2b05993a886c029c3778aa348a5daceada911b
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-16 15:39:52 -0400

    Update NotificacaoDiariaService.java

--- api-igreja/src/main/java/com/igreja/api/controller/NotificacaoDiariaService.java
+++ api-igreja/src/main/java/com/igreja/api/controller/NotificacaoDiariaService.java
@@ -6,8 +6,6 @@ import com.igreja.api.repository.MembroRepository;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
