f03ad048252f3a3db33e7c574a2dfc7e8c231c0a
Jorge Silva <jorge.silva.msbr@gmail.com>
2026-09-16 17:16:41 -0400

    Create PequenoGrupo.java

---
+++ api-igreja/src/main/java/com/igreja/api/model/PequenoGrupo.java
@@ -0,0 +1,24 @@
+package com.igreja.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import com.igreja.api.model.Membro;
+import java.util.List;
+
+@Entity
+@Table(name = "pequenoGrupo")
+@Data
+public class PequenoGrupo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private Membro lider;
+    private List<Membro> membros;
+    private String informacoes;
+    private String local;
+}
